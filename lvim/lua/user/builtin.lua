bf7aa2bda17cc1ea4afb124c8cb6eb7479b2e19f
NikSun <candysfm@gmail.com>
2022-06-13 11:31:55 +0800

    remove useless lvim builtin nvimtree options

--- lvim/lua/user/builtin.lua
+++ lvim/lua/user/builtin.lua
@@ -26,7 +26,7 @@ M.config = function()
   -- NvimTree
   -- =========================================
   lvim.builtin.nvimtree.side = "left"
-  lvim.builtin.nvimtree.show_icons.git = 0
+  -- lvim.builtin.nvimtree.show_icons.git = 0
   lvim.builtin.nvimtree.setup.open_on_setup = false
   lvim.builtin.nvimtree.setup.diagnostics = {
     enable = true,
