59547e266d4f723d6f1e77bd3eff19117ca85e8b
NikSun <candysfm@gmail.com>
2022-11-12 00:31:18 +0800

    fix

--- lvim/lua/user/null_ls.lua
+++ lvim/lua/user/null_ls.lua
@@ -4,7 +4,18 @@ M.config = function()
   local formatters = require "lvim.lsp.null-ls.formatters"
 
   formatters.setup {
-    { command = "beautysh", filetypes = { "sh" } },
+    {
+      command = "beautysh",
+      extra_args = { "--indent-size", "2" },
+    },
+  }
+
+  local linters = require "lvim.lsp.null-ls.linters"
+
+  linters.setup {
+    {
+      command = "shellcheck",
+    },
   }
 
 end
