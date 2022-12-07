de4203b0d33ba1cb5c0a787ce76521f16d69f498
NikSun <candysfm@gmail.com>
2022-12-07 11:20:44 +0800

    add clang-format support

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-BASE_PATH=$(cd "$(dirname "$0")"; pwd)
+
+BASE_PATH=$(cd "$(dirname "$0")" || exit; pwd)
 rm -f ~/.vimrc ~/.gvimrc ~/.tmux.conf ~/.zshrc ~/.zshenv
 mkdir ~/.vim
-ln -s ${BASE_PATH}/coc-settings.json ~/.vim/coc-settings.json
-ln -s ${BASE_PATH}/vimrc ~/.vimrc
-ln -s ${BASE_PATH}/gvimrc ~/.gvimrc
-ln -s ${BASE_PATH}/tern-config ~/.tern-config
-ln -s ${BASE_PATH}/editorconfig ~/.editorconfig
-ln -s ${BASE_PATH}/agignore ~/.agignore
-ln -s ${BASE_PATH}/prettierrc ~/.prettierrc
-ln -s ${BASE_PATH}/zshrc ~/.zshrc
-ln -s ${BASE_PATH}/zshenv ~/.zshenv
-ln -s ${BASE_PATH}/p10k.zsh ~/.p10k.zsh
-ln -s ${BASE_PATH}/tmux.conf ~/.tmux.conf
-ln -s ${BASE_PATH}/kitty ~/.config/
-ln -s ${BASE_PATH}/lvim ~/.config/
-ln -s ${BASE_PATH}/cargo_config ~/.cargo/config
+ln -s "$BASE_PATH/coc-settings.json" ~/.vim/coc-settings.json
+ln -s "$BASE_PATH/vimrc" ~/.vimrc
+ln -s "$BASE_PATH/gvimrc" ~/.gvimrc
+ln -s "$BASE_PATH/tern-config" ~/.tern-config
+ln -s "$BASE_PATH/editorconfig" ~/.editorconfig
+ln -s "$BASE_PATH/agignore" ~/.agignore
+ln -s "$BASE_PATH/prettierrc" ~/.prettierrc
+ln -s "$BASE_PATH/zshenv" ~/.zshenv
+ln -s "$BASE_PATH/zshrc" ~/.zshrc
+ln -s "$BASE_PATH/p10k.zsh" ~/.p10k.zsh
+ln -s "$BASE_PATH/tmux.conf" ~/.tmux.conf
+ln -s "$BASE_PATH/kitty" ~/.config/
+ln -s "$BASE_PATH/lvim" ~/.config/
+ln -s "$BASE_PATH/cargo_config" ~/.cargo/config
+ln -s "$BASE_PATH/clang-format" ~/.clang-format
