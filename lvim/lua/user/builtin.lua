243c0cdd5169cf08e9bc82fc8efce8088eaf86b1
NikSun <candysfm@gmail.com>
2022-12-07 11:00:36 +0800

    for default treesitter parser install

--- lvim/lua/user/builtin.lua
+++ lvim/lua/user/builtin.lua
@@ -62,6 +62,8 @@ M.config = function()
 
   -- if you don't want all the parsers change this to a table of the ones you want
   -- lvim.builtin.treesitter.ensure_installed = "maintained"
+  lvim.builtin.treesitter.ensure_installed = { "c", "cpp", "rust", "python", "toml", "yaml", "cmake", "make",
+    "dockerfile", "bash", "comment" }
   lvim.builtin.treesitter.ignore_install = { "haskell" }
   lvim.builtin.treesitter.highlight.enabled = true
   lvim.builtin.treesitter.indent.enable = true
