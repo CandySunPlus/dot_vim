8516db16e2dedfd3686b31511d31b911ff3c69c0
NikSun <candysfm@gmail.com>
2023-03-20 20:16:31 +0800

    update

--- lvim/lua/user/transparent.lua
+++ lvim/lua/user/transparent.lua
@@ -6,7 +6,6 @@ M.config = function()
     return
   end
   transparent.setup({
-    enable = true, -- boolean: enable transparent
     extra_groups = { -- table/string: additional groups that should be cleared
       -- In particular, when you set it to 'all', that means all available groups
 
