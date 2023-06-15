8629a422fccb4116c496c4c532ac4bd6085d97e3
NikSun <candysfm@gmail.com>
2023-06-15 20:53:12 +0800

    udpate

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,7 +1,7 @@
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
-lvim.format_on_save = true
+lvim.format_on_save = false
 lvim.lint_on_save = true
 lvim.colorscheme = "tokyonight-storm"
 -- lvim.log.level = "debug"
