a82fcbf598d3f8c7826a86a998180f89a973d5cf
sunbeiluo <sunbeiluo@meituan.com>
2023-10-10 13:04:08 +0800

    update

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -49,11 +49,34 @@ M.config = function()
       end,
     },
     {
-      "phaazon/hop.nvim",
-      event = "BufRead",
-      config = function()
-        require("user.hop").config()
-      end,
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      opts = {},
+      keys = {
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
+        {
+          "S",
+          mode = { "n", "x", "o" },
+          function() require("flash").treesitter() end,
+          desc =
+          "Flash Treesitter"
+        },
+        { "r", mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
+        {
+          "R",
+          mode = { "o", "x" },
+          function() require("flash").treesitter_search() end,
+          desc =
+          "Treesitter Search"
+        },
+        {
+          "<c-s>",
+          mode = { "c" },
+          function() require("flash").toggle() end,
+          desc =
+          "Toggle Flash Search"
+        },
+      },
     },
     {
       "j-hui/fidget.nvim",
