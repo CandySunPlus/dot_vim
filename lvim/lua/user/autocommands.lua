475a7bf733a39d7024bf0e6190d3ce12df2a57b1
sunbeiluo <sunbeiluo@meituan.com>
2023-10-11 11:01:40 +0800

    update

--- lvim/lua/user/autocommands.lua
+++ lvim/lua/user/autocommands.lua
@@ -3,13 +3,6 @@ local M = {}
 M.config = function()
   lvim.autocommands = {
     {
-      "Filetype",
-      {
-        pattern = { "vue" },
-        command = "syntax sync fromstart",
-      },
-    },
-    {
       "BufWinEnter",
       {
         pattern = { "*" },
@@ -22,70 +15,7 @@ M.config = function()
         pattern = { "*" },
         command = "silent! mkview"
       }
-    },
-    {
-      "BufRead,BufNewFile",
-      {
-        pattern = { "*.wxml" },
-        command = "setfiletype html",
-      },
-    },
-    {
-      "BufRead,BufNewFile",
-      {
-        pattern = { "*.wxss" },
-        command = "setfiletype less",
-      },
-    },
-    {
-      "BufRead,BufNewFile",
-      {
-        pattern = { "*.wxs" },
-        command = "setfiletype javascript",
-      },
-    },
-    {
-      "BufRead,BufNewFile",
-      {
-        pattern = { "*.mina" },
-        command = "setfiletype vue",
-      },
-    },
-    {
-      "BufRead,BufNewFile",
-      {
-        pattern = { "*.m" },
-        command = "setfiletype objc",
-      },
-    },
-    {
-      "BufRead,BufNewFile",
-      {
-        pattern = { "*.mm" },
-        command = "setfiletype objcpp",
-      },
-    },
-    {
-      "BufRead,BufNewFile",
-      {
-        pattern = { "*.asm" },
-        command = "setfiletype nasm",
-      },
-    },
-    {
-      "BufRead,BufNewFile",
-      {
-        pattern = { "CMakeLists.*" },
-        command = "setfiletype cmake",
-      },
-    },
-    {
-      "BufRead,BufNewFile",
-      {
-        pattern = { "/etc/nginx/*", "/usr/local/nginx/conf/*", "/usr/local/etc/nginx/*" },
-        command = "setfiletype nginx",
-      },
-    },
+    }
   }
 end
 
