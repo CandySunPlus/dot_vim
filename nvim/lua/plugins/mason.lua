782689fddc1aa046b7b4098486be31b2bda91fc4
sunbeiluo <sunbeiluo@meituan.com>
2024-05-14 17:30:33 +0800

    update

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -9,11 +9,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
-        "rust_analyzer",
         "clangd",
         "pyright",
-        "tsserver",
         -- add more arguments for adding more language servers
       })
     end,
@@ -25,7 +22,6 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "prettier",
         "shellcheck",
         "shfmt",
         -- "codespell",
