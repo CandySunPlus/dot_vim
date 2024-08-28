5aa67ceff991a07daddec6ab9a34f9f5542f0077
sunbeiluo <sunbeiluo@meituan.com>
2024-08-28 16:54:48 +0800

    add ai

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -19,5 +19,24 @@ return {
         command_palette = false
       }
     }
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    opts = {
+      provider = "openai",
+      openai = {
+        endpoint = "https://api.moonshot.cn/v1",
+        model = "moonshot-v1-8k",
+        temperature = 0.3,
+        max_tokens = 8192
+      }
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    }
   }
 }
