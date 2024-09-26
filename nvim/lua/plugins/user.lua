ed265c3e2aed2e55da3f472e341f2c0fd0d19efd
sunbeiluo <sunbeiluo@meituan.com>
2024-09-26 22:32:22 +0800

    update

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -69,11 +69,14 @@ return {
   },
   {
     "benlubas/molten-nvim",
-    version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
     build = ":UpdateRemotePlugins",
     init = function()
       -- this is an example, not a default. Please see the readme for more configuration options
       vim.g.molten_output_win_max_height = 12
+      vim.g.molten_auto_open_output = false
+      vim.g.molten_virt_lines_off_by_1 = true
+      vim.g.molten_output_virt_lines = true
+      vim.g.molten_virt_text_output = true
     end,
   },
 }
