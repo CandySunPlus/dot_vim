e2e56c31d468692f64f4c8dd3a5e76f947e1d49c
sunbeiluo <sunbeiluo@meituan.com>
2024-10-10 14:18:01 +0800

    update for nvim molten virt output

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -75,9 +75,10 @@ return {
       -- this is an example, not a default. Please see the readme for more configuration options
       vim.g.molten_output_win_max_height = 12
       vim.g.molten_auto_open_output = false
-      vim.g.molten_virt_lines_off_by_1 = true
+      -- vim.g.molten_virt_lines_off_by_1 = true
       vim.g.molten_output_virt_lines = true
       vim.g.molten_virt_text_output = true
+      vim.g.molten_output_show_more = true
     end,
   },
 }
