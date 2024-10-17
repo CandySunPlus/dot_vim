fc40cd37afe0001981d1d15cb09e14c734647914
sunbeiluo <sunbeiluo@meituan.com>
2024-10-17 13:11:06 +0800

    update

--- wezterm.lua
+++ wezterm.lua
@@ -3,12 +3,13 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback {
-  {
-    family = 'BlexMono Nerd Font Mono',
-    weight = 'Regular'
-  },
-  'Maple Mono NF CN'
+    -- 'PragmataPro Mono Liga',
+    'IosevkaCustom Nerd Font Mono',
+    'Maple Mono NF CN'
 }
+config.line_height = 1.1
+config.font_size = 13
+-- config.harfbuzz_features = { 'ss09', 'calt' }
 config.color_scheme = 'Catppuccin Mocha'
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
@@ -19,10 +20,10 @@ config.macos_window_background_blur = 70
 config.text_background_opacity = 0.9
 
 config.window_padding = {
-  left = 20,
-  right = 20,
-  top = 20,
-  bottom = 5
+    left = 5,
+    right = 5,
+    top = 5,
+    bottom = 5
 }
 
 return config
