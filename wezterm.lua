fb6b2ee46027a5c8fb58711fc49479d4d33d517f
sunbeiluo <sunbeiluo@meituan.com>
2024-10-17 16:28:22 +0800

    update

--- wezterm.lua
+++ wezterm.lua
@@ -15,9 +15,9 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.show_new_tab_button_in_tab_bar = false
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 70
-config.text_background_opacity = 0.9
+-- config.window_background_opacity = 9
+-- config.macos_window_background_blur = 70
+-- config.text_background_opacity = 0.9
 
 config.window_padding = {
     left = 5,
