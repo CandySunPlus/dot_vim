ec046944e39ac7b52023c0433a7d4425f322cf46
sunbeiluo <sunbeiluo@meituan.com>
2024-11-29 17:32:34 +0800

    fix lsp cmp bug

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,4 +1,8 @@
 local function is_visible(cmp) return cmp.core.view:visible() or vim.fn.pumvisible() == 1 end
+local function has_words_before()
+  local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
 
 ---@type LazySpec
 return {
@@ -42,6 +46,26 @@ return {
             cmp.complete()
           end
         end),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if is_visible(cmp) then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          elseif vim.api.nvim_get_mode().mode ~= "c" and vim.snippet and vim.snippet.active { direction = 1 } then
+            vim.schedule(function() vim.snippet.jump(1) end)
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if is_visible(cmp) then
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+          elseif vim.api.nvim_get_mode().mode ~= "c" and vim.snippet and vim.snippet.active { direction = -1 } then
+            vim.schedule(function() vim.snippet.jump(-1) end)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       })
     end,
   }
