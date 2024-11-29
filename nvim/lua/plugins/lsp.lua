372285580a16860bc933938655bb355293d356f4
sunbeiluo <sunbeiluo@meituan.com>
2024-11-29 10:26:46 +0800

    update nvim cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,3 +1,5 @@
+local function is_visible(cmp) return cmp.core.view:visible() or vim.fn.pumvisible() == 1 end
+
 ---@type LazySpec
 return {
   { "folke/lsp-colors.nvim", event = "BufRead" },
@@ -21,4 +23,26 @@ return {
       },
     },
   },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<C-P>"] = cmp.mapping(function()
+          if is_visible(cmp) then
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            cmp.complete()
+          end
+        end),
+        ["<C-N>"] = cmp.mapping(function()
+          if is_visible(cmp) then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            cmp.complete()
+          end
+        end),
+      })
+    end,
+  }
 }
