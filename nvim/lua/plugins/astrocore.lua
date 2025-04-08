7322a5ba47a7f3a0ee1daea0b9499976e226d86d
sunbeiluo <sunbeiluo@meituan.com>
2025-04-08 11:34:20 +0800

    update

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -10,12 +10,12 @@ return {
   opts = {
     -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 256, lines = 10000 },             -- set global limits for large files for disabling features like treesitter
-      autopairs = true,                                             -- enable autopairs at start
-      cmp = true,                                                   -- enable completion at start
+      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      autopairs = true, -- enable autopairs at start
+      cmp = true, -- enable completion at start
       diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true,                                          -- highlight URLs at start
-      notifications = true,                                         -- enable notifications at start
+      highlighturl = true, -- highlight URLs at start
+      notifications = true, -- enable notifications at start
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
@@ -24,16 +24,15 @@ return {
     },
     -- vim options can be configured here
     options = {
-      opt = {                  -- vim.opt.<key>
+      opt = { -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
-        number = true,         -- sets vim.opt.number
-        spell = false,         -- sets vim.opt.spell
-        signcolumn = "yes",    -- sets vim.opt.signcolumn to yes
-        wrap = false,          -- sets vim.opt.wrap
+        number = true, -- sets vim.opt.number
+        spell = false, -- sets vim.opt.spell
+        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+        wrap = false, -- sets vim.opt.wrap
         showbreak = " ↳  ",
         laststatus = 3,
-        guicursor =
-        "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
+        guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
@@ -81,13 +80,7 @@ return {
         ["<Leader>ml"] = { ":MoltenEvaluateLine<cr>", silent = true, desc = "Evaluate line" },
         ["<Leader>mo"] = { ":noautocmd MoltenEnterOutput<cr>", silent = true, desc = "Show or enter output" },
         ["<Leader>mc"] = { ":MoltenReevaluateCell<cr>", silent = true, desc = "Re-evaluate cell" },
-
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
-
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
+        ["<Leader>gg"] = { function() require("snacks").lazygit() end, silent = true, desc = "Lazygit" },
       },
       v = {
         ["<Leader>mr"] = { ":<C-u>MoltenEvaluateVisual<cr>gv", silent = true, desc = "Run visual selection" },
