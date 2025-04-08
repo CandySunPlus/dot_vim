ce592ddb23abc6c1c8c6feb251936c336ad40308
sunbeiluo <sunbeiluo@meituan.com>
2025-04-08 11:42:01 +0800

    update

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -12,6 +12,7 @@ return {
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.project.project-nvim" },
+  { import = "astrocommunity.recipes.vscode-icons" },
   -- { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.motion.flash-nvim" },
