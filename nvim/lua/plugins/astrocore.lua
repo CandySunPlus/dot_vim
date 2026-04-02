befe065bc32f85734d751b9fccf2a846da2f271b
sunbeiluo <sunbeiluo@meituan.com>
2026-04-02 17:13:10 +0800

    update

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -45,7 +45,7 @@ return {
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        wrap = true, -- sets vim.opt.wrap
         showbreak = " ↳  ",
         laststatus = 3,
         winborder = "bold",
